df8c5d6215caaa8d2a0c48ce7241a015dfc025bc
Jeff Spooner <jdspoone@icloud.com>
2022-02-16 13:05:35 -0700

    Initial Commit

---
+++ SwiftUIProgrammaticNavigation/SwiftUIProgrammaticNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIProgrammaticNavigationApp.swift
+//  SwiftUIProgrammaticNavigation
+//
+//  Created by Jeff Spooner on 2022-02-16.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIProgrammaticNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
